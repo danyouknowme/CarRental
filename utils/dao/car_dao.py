cea43a3a3c49f9d02e35dca2225bf71ef6198d53
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2022-04-26 20:53:26 +0700

    Add another functions to get cars data

--- utils/dao/car_dao.py
+++ utils/dao/car_dao.py
@@ -8,3 +8,18 @@ class CarDao:
 
   def get_all_cars(self):
     return self.__session.query(CarModel).all()
+
+  def get_cars_by_brand(self, brand: str):
+    return self.__session.query(CarModel).filter(CarModel.brand == brand).all()
+
+  def get_car_by_user_id(self, user_id: int):
+    return self.__session.query(CarModel).filter(CarModel.user_id == user_id).all()
+
+  def get_cars_by_sort_price(self):
+    car_list = self.get_all_cars()
+    return sorted(car_list, key=lambda car: car.price)
+
+  def create_new_rental_car(self, car_rental: CarModel):
+    self.__session.add(car_rental)
+    self.__session.commit()
+    print("Insert data to the database successfully!")
